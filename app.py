89e4c1b34641c230546c4763cca552400be232e9
Akindeji Hill <akindeji@gmail.com>
2023-01-12 19:27:24 -0500

    Session driven version

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import *
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -15,27 +15,67 @@ def root():
     return render_template("home.html", title=satisfaction_survey.title, instructions=satisfaction_survey.instructions)
 
 
-@app.route("/questions/<int:qnum>")
-def question(qnum):
-    """present a form for the current question"""
+# @app.route("/questions/get/<int:qnum>")
+# def question(qnum):
+#     """present a form for the current question"""
 
+#     q_list=satisfaction_survey.questions
+
+#     #Make sure the user hasn't skipped any questions
+#     if qnum > len(responses):
+#         flash("Please answer the questions in order.")
+#         return redirect("/questions/" + str(len(responses)))
+
+#     #Send the user forward    
+#     if qnum < len(q_list):
+#          return render_template("question_form.html", choices=q_list[qnum].choices, current_question=q_list[qnum].question, title=satisfaction_survey.title, instructions=satisfaction_survey.instructions, q_num = qnum)
+#     else:
+#         return render_template("finnished.html", answers=responses)     
+
+
+@app.route("/questions/session", methods = ['POST'])
+def survey_session():
+    """starting point for session driven survey"""
+    print("POST recieved")
+    print(request.form)
+    session["responses"] = []
+    session["next_question_num"] = 0
+    return redirect("/questions/post")
+
+
+@app.route("/questions/post", methods = ['POST', 'GET'])
+def post_question():
+    """present a form for the current question using POST method"""
+
+    qnum = session["next_question_num"]
     q_list=satisfaction_survey.questions
 
-    #Make sure the user hasn't skipped any questions
-    if qnum > len(responses):
-        flash("Please answer the questions in order.")
-        return redirect("/questions/" + str(len(responses)))
+    print(f"***** Response was: {request.form.get('response')}")
 
-    # #make sure the user can't go back once the survey is complete
-    # if len(responses) >= len(q_list):
-    #     flash("Sorry, you can't change your answers once the survey is complete.")
-    #     return render_template("finnished.html", answers=responses)  
+    #update responses session
+    if request.form.get("response"):
+        new_response = request.form["response"]
+        responses = session["responses"]
+        responses.append(new_response)
+        session["responses"] = responses #rebind the name in the session
+
+    #update next question number
+    qnum = len(session["responses"])
+    print(f"Next question number is: {qnum}")
+    print(f"There are now {str(len(session['responses']))} responses")
+    session["next_question_num"] = qnum
 
     #Send the user forward    
     if qnum < len(q_list):
-         return render_template("question_form.html", choices=q_list[qnum].choices, current_question=q_list[qnum].question, title=satisfaction_survey.title, instructions=satisfaction_survey.instructions, q_num = qnum)
+        session["choices"] = q_list[qnum].choices
+        session["current_question"] = q_list[qnum].question
+        session["title"] = satisfaction_survey.title
+        session["instructions"] = satisfaction_survey.instructions
+        return render_template("question_form_session.html")
     else:
-        return render_template("finnished.html", answers=responses)     
+        return render_template("finnished_session.html") 
+
+
 
 
 @app.route("/questions/submit")
